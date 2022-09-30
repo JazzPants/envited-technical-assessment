67d716f5fa51b1fece8c0bdaf744cd46ddcaa686
Leighton <leighton.l14@gmail.com>
2022-10-01 09:38:17 +1000

    Commit 6 - Linked up css files to jsx files

--- src/components/createEvent/createEvent.jsx
+++ src/components/createEvent/createEvent.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./createEvent.css";
 
 const CreateEvent = () => {
-  return <div>createEvent</div>;
+  return <div>I'm createEvent</div>;
 };
 
 export default CreateEvent;
--- src/components/eventDetails/eventDetails.jsx
+++ src/components/eventDetails/eventDetails.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./eventDetails.css";
 
 const EventDetails = () => {
-  return <div>eventDetails</div>;
+  return <div>I'm eventDetails</div>;
 };
 
 export default EventDetails;
