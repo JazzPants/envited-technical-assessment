d49a360a02c4838c163574b9f976d301c121a97e
Leighton <leighton.l14@gmail.com>
2022-10-01 10:51:28 +1000

    Commit 9 - Landing page basic structure created

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,18 +1,35 @@
 import React from "react";
 import "./landing.css";
 import { Link } from "react-router-dom";
+import LandingPageImage from "../../img/landingPageImage";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Hello there. Welcome to envited.</h1>
-      <nav>
-        <Link to="/create">Create an event</Link>
-      </nav>
-      <nav>
-        <Link to="/event">Event Details</Link>
-      </nav>
-    </div>
+    <>
+      <div className="landing">
+        <div className="landing-left-wrapper">
+          <div className="landing-left-image">
+            <LandingPageImage />
+          </div>
+        </div>
+        <div className="landing-right-wrapper">
+          <div className="landing-right-content">
+            <h1>Imagine if SnapChat had events.</h1>
+            <p>
+              Easily host and share events with your friends across any social
+              media.
+            </p>
+            <p>Button</p>
+            <nav>
+              <Link to="/create">Create my event</Link>
+            </nav>
+            <nav>
+              <Link to="/event">Event Details</Link>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
