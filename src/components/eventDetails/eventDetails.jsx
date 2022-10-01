a703f2d43d7fd3e72b7aa97cda7de828b1d574e3
Leighton <leighton.l14@gmail.com>
2022-10-01 12:48:55 +1000

    Commit 12 - Added text gradient span, fixed some buttons, added paper to event details

--- src/components/eventDetails/eventDetails.jsx
+++ src/components/eventDetails/eventDetails.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./eventDetails.css";
 import { Link } from "react-router-dom";
+import { Button, Paper } from "@mui/material";
 
 const EventDetails = () => {
   return (
     <>
-      <h2>I'm eventDetails</h2>
+      <h2>Create an Event - Event Details</h2>
+      <Paper
+        className="event-details"
+        style={{ backgroundColor: "#EDE5FF" }}
+      ></Paper>
       <nav>
-        <Link to="/">Home</Link>
+        <Link to="/">
+          <Button id="home-button-hello">Home</Button>
+        </Link>
+      </nav>
+      <nav>
+        <Link to="/create">
+          <Button id="back-button">Back</Button>
+        </Link>
       </nav>
     </>
   );
